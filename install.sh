0c6e258ffe54be92396ba681598b4d339aa50251
The Fake Slim Shady <74916637+Sid220@users.noreply.github.com>
2021-12-01 14:55:26 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,8 @@
-
+#!/bin/zsh
+sudo apt-get install -y ufw
+if [ $? -ne 0 ]; then
+  echo "Failed to install ufw"
+  exit 1
+fi
+mkdir /serv/pkgs/fw
+git clone https://github.com/Sid220/servbuntu-fw-app.git /serv/pkgs/fw/
